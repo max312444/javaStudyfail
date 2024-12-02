1dec121a2ef9b3fb87b9b2b903518ec6a0238077
max312444 <max12max@g.yju.ac.kr>
2024-12-02 21:16:55 +0900

    12/02 commit

--- src/yyy.java
+++ src/yyy.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class xxx {
-    //         학생 목록 출력 함수
+public class yyy {
     static void prtMatrix(float[][] argMatrix, int numbers) {
-        String[] pritingPart = {"학번", "국어", "영어", "수학", "합계", "평균"};
+        String[] printPart = {"학번", "국어", "영어", "수학", "합계", "평균"};
         if (numbers == 0) {
             System.out.println("입력된 학생 정보가 없습니다.");
             return;
         }
         for (int i = 0 ; i < numbers ; i++) {
             for (int j = 0 ; j < argMatrix[i].length ; j++) {
-                System.out.print(pritingPart[j] + "" +
+                System.out.print(printPart[j] + "" +
                         "" +
                         "" +
                         "" +
@@ -21,15 +20,14 @@ public class xxx {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 사용자에게 메뉴를 출력하여 입력에 따라 다양한 동작을 수행하는 프로그램 작성
-        // 프로그램은 종료 명령을 받을 때 까지 반복 실행
-        // 학생 성적 순서대로 넣기 위한 카운트
-        int count = 0;
-        int userInput = 0;
-        final int NUM_OF_ROW = 3;
+        // 초기 배열 크기 3행 6열
+        int NUM_OF_ROW = 3;
         final int NUM_OF_COL = 6;
-
-        // 학생 성적 저장용 배열
+        // 학생수 카운트
+        int studentCount = 0;
+        int userInput = 0;
+        String usedID = "";
+        // 학생 성적 저장용 배열 생성
         float[][] studentScore = new float[NUM_OF_ROW][NUM_OF_COL];
 
         while (true) {
@@ -43,45 +41,75 @@ public class xxx {
             System.out.println("=============================");
 
             while (true) {
-                // 사용자 입력
                 System.out.print("선택 : ");
                 userInput = sc.nextInt();
-                // 입력 값이 메뉴 내에 있는지 판별
                 if (userInput > 0 && userInput < 5) {
                     break;
                 } else {
-                    System.out.println("잘못된 값입니다. 다시 입력해주세요");
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
                 }
             }
-            // 4번 선택시 프로그램 종료
             if (userInput == 4) {
-                System.out.println("프로그램을 종료합니다.");
+                System.out.println("프로그램 종료");
                 break;
             }
+            // 배열 크기를 늘릴 때 전에 있던 데이터 저장용 배열 생성
+            float[][] chageSize = new float[NUM_OF_ROW][NUM_OF_COL];
 
             switch (userInput) {
                 // 학생 성적 입력
                 case 1:
+                    if (studentCount > studentScore.length - 1) {
+                        for (int i = 0 ; i < studentScore.length ; i++) {
+                            for (int j = 0; j < studentScore[i].length; j++) {
+                                chageSize[i][j] = studentScore[i][j];
+                            }
+                        }
+                        studentScore = new float[NUM_OF_ROW * 2][NUM_OF_COL];
+                        System.out.println("배열이 확장되었습니다. 새로운 크기: " + NUM_OF_ROW);
+                        for (int i = 0 ; i < chageSize.length ; i++) {
+                            for (int j = 0; j < chageSize[i].length; j++) {
+                                studentScore[i][j] = chageSize[i][j];
+                            }
+                        }
+                    }
                     System.out.print("학번을 입력하세요 : ");
-                    studentScore[count][0] = sc.nextFloat();
+                    studentScore[studentCount][0] = sc.nextFloat();
+                    for (int i = 0 ; i < studentScore.length ; i++) {
+                        if (studentScore[i][0] == studentScore[studentCount][0]) {
+                            System.out.println("중복된 학번입니다.");
+                            System.out.print("덮어쓰기를 희망합니까? (Y: 덮어쓰기 진행, q: 메뉴로 돌아가기): ");
+                            usedID = sc.nextLine();
+                            if (usedID == "Y") {
+                                for (int k = 0 ; k < studentScore.length ; k++) {
+                                    for (int j = 0; j < studentScore[i].length; j++) {
+                                        chageSize[k][j] = studentScore[k][j];
+                                    }
+                                }
+                            } else if (usedID == "q") {
+                                System.out.println("입력이 취소되었습니다. 메뉴로 돌아갑니다.");
+                                break;
+                            }
+                        }
+                    }
                     // 입력된 학번의 성적을 차례로 입력 - 배열에 저장 (배열의 자료형은 float형이다.)
                     // 합계와 평균 계산하여 해당 필드에 저장
                     System.out.print("국어 성적 : ");
-                    studentScore[count][1] = sc.nextFloat();
+                    studentScore[studentCount][1] = sc.nextFloat();
                     System.out.print("영어 성적 : ");
-                    studentScore[count][2] = sc.nextFloat();
+                    studentScore[studentCount][2] = sc.nextFloat();
                     System.out.print("수학 성적 : ");
-                    studentScore[count][3] = sc.nextFloat();
+                    studentScore[studentCount][3] = sc.nextFloat();
                     System.out.println("입력이 완료되었습니다.\n");
                     // 합계
-                    studentScore[count][4] = studentScore[count][1] + studentScore[count][2] + studentScore[count][3] ;
+                    studentScore[studentCount][4] = studentScore[studentCount][1] + studentScore[studentCount][2] + studentScore[studentCount][3] ;
                     // 평균
-                    studentScore[count][5] = studentScore[count][4] / 3.0f;
-                    count++;
+                    studentScore[studentCount][5] = studentScore[studentCount][4] / 3.0f;
+                    studentCount++;
                     break;
                 // 입력된 학생 목록 출력
                 case 2:
-                    prtMatrix(studentScore, count);
+                    prtMatrix(studentScore, studentCount);
                     break;
                 // 학생 삭제하기
                 case 3:
@@ -119,14 +147,15 @@ public class xxx {
                     }
                     // 삭제 완료 후 업데이트된 목록 출력
                     System.out.println("삭제가 완료되었습니다.");
-                    prtMatrix(studentScore, count);
-                    count--;
+                    prtMatrix(studentScore, studentCount);
+                    studentCount--;
                     break;
 
                 default:
                     System.out.println("Something went wrong");
                     break;
             }
+
         }
     }
 }
